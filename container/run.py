36684f7eff59f983e6fd52f5f2d86ed5eeeb913d
Brian Sipos <brian.sipos@jhuapl.edu>
2024-12-01 21:39:13 -0500

    Using separated PkiCa class for PKIX

--- container/run.py
+++ container/run.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3
-#
+#!/usr/bin/env python3
+''' An orchestrator for starting networks within containers.
+
+The directory ./workdir is used as a test file staging area.
+'''
+
 import argparse
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
@@ -7,6 +11,7 @@ from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, ec
 import asn1
 import datetime
+import ipaddress
 import logging
 import os
 import re
@@ -14,23 +19,208 @@ import shutil
 import subprocess
 import sys
 import time
+from typing import Union, Optional
 import yaml
 
 LOGGER = logging.getLogger()
 SELFDIR = os.path.dirname(os.path.abspath(__file__))
 
+DEFAULT_STAGEDIR = os.path.join(SELFDIR, 'workdir')
 
-def generate_key(key_opts):
-    keytype = key_opts.get('keytype', 'SECP256R1').upper()
-    if keytype == 'RSA':
-        key_size = 1024
-        node_key = rsa.generate_private_key(65537, key_size, backend=default_backend())
-    elif keytype.startswith('SECP'):
-        curve = getattr(ec, keytype)
-        node_key = ec.generate_private_key(curve, backend=default_backend())  # Curve for COSE ES256
-    else:
-        raise ValueError(f'Unknown keytype: {keytype}')
-    return node_key
+
+class PkiCa:
+    ''' A local software PKI CA generator. '''
+
+    def __init__(self):
+        self._nowtime = datetime.datetime.now(datetime.timezone.utc)
+        self._ca_key = None
+        self._ca_cert = None
+
+    def other_name_eid(self, eid:str) -> x509.OtherName:
+        ''' Encode a text EID as an Other Name object.
+        '''
+        eid_enc = asn1.Encoder()
+        eid_enc.start()
+        eid_enc.write(eid.encode('ascii'), asn1.Numbers.IA5String)
+        return x509.OtherName(
+            x509.oid.ObjectIdentifier('1.3.6.1.5.5.7.8.11'),  # id-on-bundleEID
+            eid_enc.output()
+        )
+
+    def generate_key(self, key_opts:dict) -> Union[rsa.RSAPrivateKey, ec.EllipticCurvePrivateKey]:
+        keytype = key_opts.get('keytype', 'SECP256R1').upper()
+        if keytype == 'RSA':
+            key_size = 1024
+            node_key = rsa.generate_private_key(65537, key_size, backend=default_backend())
+        elif keytype.startswith('SECP'):
+            curve = getattr(ec, keytype)
+            node_key = ec.generate_private_key(curve, backend=default_backend())  # Curve for COSE ES256
+        else:
+            raise ValueError(f'Unknown keytype: {keytype}')
+        return node_key
+
+    def generate_root_ca(self, certfile:str, keyfile:str) -> x509.Certificate:
+        ''' Generate and retain a root CA. '''
+        ca_key = self.generate_key({})
+
+        ca_name = x509.Name([
+            x509.NameAttribute(x509.oid.NameOID.COMMON_NAME, 'Certificate Authority'),
+        ])
+        ca_cert = x509.CertificateBuilder().subject_name(
+            ca_name
+        ).issuer_name(
+            ca_name
+        ).public_key(
+            ca_key.public_key()
+        ).serial_number(
+            x509.random_serial_number()
+        ).not_valid_before(
+            self._nowtime
+        ).not_valid_after(
+            self._nowtime + datetime.timedelta(days=10)
+        ).add_extension(
+            x509.BasicConstraints(ca=True, path_length=1),
+            critical=True,
+        ).add_extension(
+            x509.KeyUsage(
+                digital_signature=False,
+                content_commitment=False,
+                key_encipherment=False,
+                data_encipherment=False,
+                key_agreement=False,
+                key_cert_sign=True,
+                crl_sign=True,
+                encipher_only=False,
+                decipher_only=False,
+            ),
+            critical=True,
+        ).add_extension(
+            # This is mandated by some browser interpretations of chain validation
+            x509.ExtendedKeyUsage([
+                x509.oid.ExtendedKeyUsageOID.CLIENT_AUTH,
+                x509.oid.ExtendedKeyUsageOID.SERVER_AUTH,
+                x509.oid.ObjectIdentifier('1.3.6.1.5.5.7.3.35')  # id-kp-bundleSecurity
+            ]),
+            critical=False,
+         ).add_extension(
+            x509.NameConstraints(
+                permitted_subtrees=[
+                    self.other_name_eid('ipn:*.*'),
+                ],
+                excluded_subtrees=None,
+            ),
+            critical=True,
+         ).add_extension(
+            x509.SubjectKeyIdentifier.from_public_key(ca_key.public_key()),
+            critical=False,
+        ).add_extension(
+            x509.AuthorityKeyIdentifier.from_issuer_public_key(ca_key.public_key()),
+            critical=False,
+        ).sign(ca_key, hashes.SHA256(), backend=default_backend())
+
+        os.makedirs(os.path.dirname(keyfile), exist_ok=True)
+        with open(keyfile, 'wb') as outfile:
+            outfile.write(ca_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption(),
+            ))
+        os.makedirs(os.path.dirname(certfile), exist_ok=True)
+        with open(certfile, 'wb') as outfile:
+            outfile.write(ca_cert.public_bytes(serialization.Encoding.PEM))
+
+        self._ca_key = ca_key
+        self._ca_cert = ca_cert
+
+    def generate_end_entity(self, cafile:str, certfile:str, keyfile:str, mode:str, nodeid:str, fqdn:Optional[str]=None) -> x509.Certificate:
+        '''
+        :param mode: Either 'transport' or 'signing'.
+        :param nodeid: The Node ID for the entity as a URI string.
+        :param fqdn: For transport mode, the FQDN of the node.
+        '''
+
+        sans = [
+            self.other_name_eid(nodeid)
+        ]
+        key_usage = dict(
+            digital_signature=False,
+            content_commitment=False,
+            key_encipherment=False,
+            data_encipherment=False,
+            key_agreement=False,
+            key_cert_sign=False,
+            crl_sign=False,
+            encipher_only=False,
+            decipher_only=False,
+        )
+        ekus = [
+            x509.oid.ObjectIdentifier('1.3.6.1.5.5.7.3.35')  # id-kp-bundleSecurity
+        ]
+
+        node_key = self.generate_key({})
+
+        if mode == 'transport':
+            sans += [
+                x509.DNSName(fqdn),
+            ]
+            key_usage['digital_signature'] = True
+            ekus += [
+                x509.oid.ExtendedKeyUsageOID.CLIENT_AUTH,
+                x509.oid.ExtendedKeyUsageOID.SERVER_AUTH,
+            ]
+        elif mode == 'signing':
+            key_usage['digital_signature'] = True
+        elif mode == 'encryption':
+            key_usage['key_agreement'] = True
+
+        node_cert = x509.CertificateBuilder().subject_name(
+            x509.Name([
+                x509.NameAttribute(x509.oid.NameOID.COMMON_NAME, nodeid),
+            ]),
+        ).issuer_name(
+            self._ca_cert.subject
+        ).public_key(
+            node_key.public_key()
+        ).serial_number(
+            x509.random_serial_number()
+        ).not_valid_before(
+            self._nowtime
+        ).not_valid_after(
+            self._nowtime + datetime.timedelta(days=10)
+        ).add_extension(
+            x509.BasicConstraints(ca=False, path_length=None),
+            critical=True,
+        ).add_extension(
+            x509.SubjectAlternativeName(sans),
+            critical=False,
+        ).add_extension(
+            x509.KeyUsage(**key_usage),
+            critical=True,
+        ).add_extension(
+            x509.ExtendedKeyUsage(ekus),
+            critical=False,
+        ).add_extension(
+            x509.SubjectKeyIdentifier.from_public_key(node_key.public_key()),
+            critical=False,
+        ).add_extension(
+            x509.AuthorityKeyIdentifier.from_issuer_public_key(self._ca_key.public_key()),
+            critical=False,
+        ).sign(self._ca_key, hashes.SHA256(), backend=default_backend())
+
+        os.makedirs(os.path.dirname(keyfile), exist_ok=True)
+        with open(keyfile, 'wb') as outfile:
+            outfile.write(node_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption(),
+            ))
+        os.makedirs(os.path.dirname(certfile), exist_ok=True)
+        with open(certfile, 'wb') as outfile:
+            outfile.write(node_cert.public_bytes(serialization.Encoding.PEM))
+        if cafile:
+            os.makedirs(os.path.dirname(cafile), exist_ok=True)
+            with open(cafile, 'wb') as outfile:
+                outfile.write(self._ca_cert.public_bytes(serialization.Encoding.PEM))
 
 
 class Runner:
@@ -60,7 +250,7 @@ class Runner:
             img_name_tag = 'dtn-demo'
 
             LOGGER.info('Building image...')
-            cmd = ['build', '-t', img_name_tag, '.', '-f', 'container/Dockerfile']
+            cmd = ['build', '-t', img_name_tag, '.', '-f', os.path.join(SELFDIR, 'Dockerfile')]
             if self.args.image_no_cache:
                 cmd += ['--no-cache']
             self.run_docker(cmd)
@@ -79,153 +269,50 @@ class Runner:
                     cmd += ['-o', f'com.docker.network.bridge.name=br-{net_name}']
                     self.run_docker(cmd)
 
-            nowtime = datetime.datetime.now(datetime.timezone.utc)
+            pki = PkiCa()
 
-            os.makedirs(os.path.join('container', 'workdir'), exist_ok=True)
+            os.makedirs(DEFAULT_STAGEDIR, exist_ok=True)
 
             # Private CA
-            ca_key = generate_key({})
-            with open(os.path.join('container', 'workdir', 'ca.key'), 'wb') as outfile:
-                outfile.write(ca_key.private_bytes(
-                    encoding=serialization.Encoding.PEM,
-                    format=serialization.PrivateFormat.TraditionalOpenSSL,
-                    encryption_algorithm=serialization.NoEncryption(),
-                ))
-            ca_name = x509.Name([
-                x509.NameAttribute(x509.oid.NameOID.COMMON_NAME, 'Certificate Authority'),
-            ])
-            ca_cert = x509.CertificateBuilder().subject_name(
-                ca_name
-            ).issuer_name(
-                ca_name
-            ).public_key(
-                ca_key.public_key()
-            ).serial_number(
-                x509.random_serial_number()
-            ).not_valid_before(
-                nowtime
-            ).not_valid_after(
-                nowtime + datetime.timedelta(days=10)
-            ).add_extension(
-                x509.BasicConstraints(ca=True, path_length=1),
-                critical=True,
-            ).add_extension(
-                x509.KeyUsage(
-                    digital_signature=False,
-                    content_commitment=False,
-                    key_encipherment=False,
-                    data_encipherment=False,
-                    key_agreement=False,
-                    key_cert_sign=True,
-                    crl_sign=True,
-                    encipher_only=False,
-                    decipher_only=False,
-                ),
-                critical=False,
-            ).add_extension(
-                x509.SubjectKeyIdentifier.from_public_key(ca_key.public_key()),
-                critical=False,
-            ).add_extension(
-                x509.AuthorityKeyIdentifier.from_issuer_public_key(ca_key.public_key()),
-                critical=False,
-            ).sign(ca_key, hashes.SHA256(), backend=default_backend())
+            ca_key = pki.generate_key({})
+            pki.generate_root_ca(
+                certfile=os.path.join(DEFAULT_STAGEDIR, 'ca', 'ca.crt'),
+                keyfile=os.path.join(DEFAULT_STAGEDIR, 'ca', 'ca.key'),
+            )
 
             for (node_name, node_opts) in self._config['nodes'].items():
                 fqdn = node_name + '.local'
 
-                config_path = os.path.join('container', 'workdir', node_name, 'xdg', 'dtn')
-                if not os.path.isdir(config_path):
-                    os.makedirs(config_path)
+                config_path = os.path.join(DEFAULT_STAGEDIR, node_name, 'xdg', 'dtn')
+                os.makedirs(config_path, exist_ok=True)
 
-                log_path = os.path.join('container', 'workdir', node_name, 'log')
-                if not os.path.isdir(log_path):
-                    os.makedirs(log_path)
-
-                with open(os.path.join(config_path, 'ca.crt'), 'wb') as outfile:
-                    outfile.write(ca_cert.public_bytes(serialization.Encoding.PEM))
-
-                # Generate node keys
-                eid_enc = asn1.Encoder()
-                eid_enc.start()
-                eid_enc.write('dtn://{}/'.format(node_name).encode('ascii'), asn1.Numbers.IA5String)
-                sans = [
-                    x509.OtherName(
-                        x509.oid.ObjectIdentifier('1.3.6.1.5.5.7.8.11'),  # id-on-bundleEID
-                        eid_enc.output()
-                    ),
-                ]
-                ekus = [
-                    x509.oid.ObjectIdentifier('1.3.6.1.5.5.7.3.35')  # id-kp-bundleSecurity
-                ]
-                for key_name in ('transport', 'sign'):
-                    key_opts = node_opts.get('keys', {}).get(key_name, {})
-                    node_key = generate_key(key_opts)
-                    with open(os.path.join(config_path, f'{key_name}.key'), 'wb') as outfile:
-                        outfile.write(node_key.private_bytes(
-                            encoding=serialization.Encoding.PEM,
-                            format=serialization.PrivateFormat.TraditionalOpenSSL,
-                            encryption_algorithm=serialization.NoEncryption(),
-                        ))
-
-                    if key_name == 'transport':
-                        sans += [
-                            x509.DNSName(fqdn),
-                        ]
-                        ekus += [
-                            x509.oid.ExtendedKeyUsageOID.CLIENT_AUTH,
-                            x509.oid.ExtendedKeyUsageOID.SERVER_AUTH,
-                        ]
-
-                    node_cert = x509.CertificateBuilder().subject_name(
-                        x509.Name([
-                            x509.NameAttribute(x509.oid.NameOID.COMMON_NAME, node_name),
-                        ]),
-                    ).issuer_name(
-                        ca_cert.subject
-                    ).public_key(
-                        node_key.public_key()
-                    ).serial_number(
-                        x509.random_serial_number()
-                    ).not_valid_before(
-                        nowtime
-                    ).not_valid_after(
-                        nowtime + datetime.timedelta(days=10)
-                    ).add_extension(
-                        x509.BasicConstraints(ca=False, path_length=None),
-                        critical=True,
-                    ).add_extension(
-                        x509.SubjectAlternativeName(sans),
-                        critical=False,
-                    ).add_extension(
-                        x509.KeyUsage(
-                            digital_signature=True,
-                            content_commitment=False,
-                            key_encipherment=False,
-                            data_encipherment=False,
-                            key_agreement=False,
-                            key_cert_sign=False,
-                            crl_sign=False,
-                            encipher_only=False,
-                            decipher_only=False,
-                        ),
-                        critical=False,
-                    ).add_extension(
-                        x509.ExtendedKeyUsage(ekus),
-                        critical=False,
-                    ).add_extension(
-                        x509.SubjectKeyIdentifier.from_public_key(node_key.public_key()),
-                        critical=False,
-                    ).add_extension(
-                        x509.AuthorityKeyIdentifier.from_issuer_public_key(ca_key.public_key()),
-                        critical=False,
-                    ).sign(ca_key, hashes.SHA256(), backend=default_backend())
-                    with open(os.path.join(config_path, f'{key_name}.crt'), 'wb') as outfile:
-                        outfile.write(node_cert.public_bytes(serialization.Encoding.PEM))
+                log_path = os.path.join(DEFAULT_STAGEDIR, node_name, 'log')
+                os.makedirs(log_path, exist_ok=True)
 
                 extconfig = node_opts.get('config', {})
                 use_ipv4 = node_opts.get('use_ipv4', True)
                 use_ipv6 = node_opts.get('use_ipv6', True)
                 nodeid = 'dtn://{}/'.format(node_name)
+
+                tls_enable = extconfig.get('tls_enable', False)
+                dtls_enable = extconfig.get('dtls_enable_tx', False)
+                if tls_enable or dtls_enable:
+                    pki.generate_end_entity(
+                        cafile=os.path.join(config_path, 'ca.crt'),
+                        certfile=os.path.join(config_path, 'transport.crt'),
+                        keyfile=os.path.join(config_path, 'transport.key'),
+                        mode='transport',
+                        nodeid=nodeid,
+                        fqdn=fqdn,
+                    )
+
+                pki.generate_end_entity(
+                    cafile=os.path.join(config_path, 'ca.crt'),
+                    certfile=os.path.join(config_path, 'sign.crt'),
+                    keyfile=os.path.join(config_path, 'sign.key'),
+                    mode='sign',
+                    nodeid=nodeid,
+                )
 
                 udpcl_listen = []
                 if extconfig.get('udpcl_listen', True):
@@ -280,7 +367,7 @@ class Runner:
                         'bus_service': 'org.ietf.dtn.node.udpcl',
                         'node_id': nodeid,
 
-                        'dtls_enable_tx': False,
+                        'dtls_enable_tx': dtls_enable,
                         'dtls_ca_file': '/etc/xdg/dtn/ca.crt',
                         'dtls_cert_file': '/etc/xdg/dtn/transport.crt',
                         'dtls_key_file': '/etc/xdg/dtn/transport.key',
@@ -294,7 +381,7 @@ class Runner:
                         'bus_service': 'org.ietf.dtn.node.tcpcl',
                         'node_id': nodeid,
 
-                        'tls_enable': extconfig.get('tls_enable', False),
+                        'tls_enable': tls_enable,
                         'tls_ca_file': '/etc/xdg/dtn/ca.crt',
                         'tls_cert_file': '/etc/xdg/dtn/transport.crt',
                         'tls_key_file': '/etc/xdg/dtn/transport.key',
