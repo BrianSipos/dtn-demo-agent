d571687f3b79f5a5de624ef2d1f5de29a5c6bca1
Brian Sipos <brian.sipos@gmail.com>
2026-01-06 11:55:32 -0500

    Revert config change

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # (value 'bysource'). The default is alphabetical.
 autodoc_member_order = 'bysource'
 
-autodoc_inherit_docstrings = False
 autodoc_typehints = 'none'
 autoclass_content = 'both'
 
